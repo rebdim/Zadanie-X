9f99529daeb959dfefe17d6d36d4172029e81241
wsr7 <wsr7@college.edu>
2022-04-11 13:33:31 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            double b = 11;
+            var c = 2;
+            Math.Sqrt(a);
+            Console.WriteLine(12%4);
+            Console.WriteLine(a) ; 
+            Console.ReadKey();
+        }
+    }
+}
